9a37eb2d0543e7d80c5f63b57c06a37293c9e2a2
tannert44 <tannert8155@gmail.com>
2015-10-03 16:52:58 -0500

    Two-way data binding is working starting on three-way issue with header atm

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -1,8 +1,74 @@
 app.controller( 'ProfileCtrl', [
   '$scope',
   '$firebaseObject',
-  function($scope, $firebaseObject){
- 
+  '$compile',
+  function($scope, $firebaseObject, $compile){
+
+    var ref = new Firebase("https://xhub.firebaseio.com/");
+
+    var authProvider = ref.getAuth().uid;
+
+    var userRef = new Firebase("https://xhub.firebaseio.com/profiles/" + authProvider);
+
+    console.log("AUTHPROVIDER", authProvider);
+
+    $scope.obj = $firebaseObject(userRef);
+    
+    $scope.userName = "Enter Your Name Here";
+
+    $scope.userNameInput = false;
+
+    $scope.aboutYou = "Enter some information about yourself";
+
+    $scope.aboutYouInput = false;
+
+    $scope.business = "Enter some information about your job or buisnness";
+
+    $scope.businessInput = false;
+
+    $scope.changeUserBool =  function(){
+      console.log("Yup it works", $scope.userNameInput);
+
+      if($scope.userNameInput){
+        $scope.userNameInput = false;
+        console.log("Should Be False", $scope.userNameInput);
+      }else{
+        $scope.userNameInput = true;
+        console.log("Should Be True", $scope.userNameInput);
+      }
+
+    };
+
+    $scope.userChange = function(){
+      $scope.obj.userName = $scope.userName;
+      $scope.obj.$save();
+    }
+
+    $scope.changeAboutBool =  function(){
+      console.log("Yup it works", $scope.userNameInput);
+
+      if($scope.aboutYouInput){
+        $scope.aboutYouInput = false;
+        console.log("Should Be False1", $scope.userNameInput);
+      }else{
+        $scope.aboutYouInput = true;
+        console.log("Should Be True2", $scope.userNameInput);
+      }
+
+    };
+
+    $scope.changeBusinessBool =  function(){
+      console.log("Yup it works", $scope.userNameInput);
+
+      if($scope.businessInput){
+        $scope.businessInput = false;
+        console.log("Should Be False1", $scope.userNameInput);
+      }else{
+        $scope.businessInput = true;
+        console.log("Should Be True2", $scope.userNameInput);
+      }
+
+    };
   }
 
 ]);
