ff62066171d689e43cf350be3aba22f783808b91
tannert44 <tannert8155@gmail.com>
2015-10-05 11:50:28 -0500

    Recipes adding to profile under appropriate section. Starting Browsing pages

--- app/controllers/BrowseCtrl.js
+++ app/controllers/BrowseCtrl.js
@@ -1,8 +1,25 @@
 app.controller( 'BrowseCtrl', [
   '$scope',
-  '$firebaseObject',
-  function($scope, $firebaseObject){
- 
+  '$firebaseArray',
+  function($scope, $firebaseArray){
+    var userRef = new Firebase("https://xhub.firebaseio.com/profiles/");
+
+    var techRef = new Firebase("https://xhub.firebaseio.com/techniques/");
+
+    var recRef = new Firebase("https://xhub.firebaseio.com/recipes/");
+
+    var profileArr = $firebaseArray(userRef);
+
+    var techArr = $firebaseArray(techRef);
+
+    var recArr = $firebaseArray(recRef);
+
+    console.log("PROFILEARR", profileArr);
+
+    console.log("TECHARR", techArr);
+
+    console.log("RECARR", recArr);
+
   }
 
 ]);
--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -12,11 +12,21 @@ app.controller( 'ProfileCtrl', [
 
     var techRef = new Firebase("https://xhub.firebaseio.com/techniques/" + authProvider);
 
+    var recRef = new Firebase("https://xhub.firebaseio.com/recipes/" + authProvider);
+
     console.log("AUTHPROVIDER", authProvider);
 
     $scope.obj = $firebaseObject(userRef);
 
     $scope.techArr = $firebaseArray(techRef);
+
+    var recArr = $firebaseArray(recRef);
+
+    $scope.recPubArr = [];
+
+    $scope.recPriArr = [];
+
+    console.log("RECIPESARRAY",$scope.recPubArr);
     
     $scope.userName = "Click To Enter Your Name.";
 
@@ -29,6 +39,20 @@ app.controller( 'ProfileCtrl', [
     $scope.business = "Click to enter some information about your job or buisnness.";
 
     $scope.businessInput = false;
+
+    console.log(recArr);
+    recArr.$loaded()
+    .then(function(){
+        angular.forEach(recArr, function(value, key) {
+          if(value.availability === "true"){
+            $scope.recPubArr.push(value);
+            console.log("RECPUBARR", $scope.recPubArr);
+          }else{
+            $scope.recPriArr.push(value);
+            console.log("RECPUBARR", $scope.recPriArr);
+          }
+        });
+    });
 
 
     $scope.changeUserBool =  function(){
@@ -44,9 +68,17 @@ app.controller( 'ProfileCtrl', [
 
     };
 
-    $scope.userChange = function(){
+    $scope.userNameChange = function(){
       $scope.obj.userName = $scope.userName;
+      $scope.obj.$save();
+    };
+
+    $scope.aboutYouChange = function(){
       $scope.obj.aboutYou = $scope.aboutYou;
+      $scope.obj.$save();
+    };
+
+    $scope.businessChange = function(){
       $scope.obj.business = $scope.business;
       $scope.obj.$save();
     };
